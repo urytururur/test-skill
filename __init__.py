87fbf751dab227d2981e880d54aef2bea2b4a38a
urytururur <44616408+urytururur@users.noreply.github.com>
2019-12-14 21:06:55 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -7,12 +7,12 @@ class Test(MycroftSkill):
 
     @intent_file_handler('on.intent')
     def handle_on(self, message):
-        requests.put('http://192.168.0.101/api/AmE3DbmI-V6GpnsBHqdG-g5NG7Xiz1AvUh8iMVt7/lights/12/state', data="{\"on\":true}")
+        requests.put('http://192.168.0.106/1', data="{\"on\":true}")
         self.speak_dialog('on')
 
     @intent_file_handler('test.intent')
     def handle_off(self, message):
-        requests.put('http://192.168.0.101/api/AmE3DbmI-V6GpnsBHqdG-g5NG7Xiz1AvUh8iMVt7/lights/12/state', data="{\"on\":false}")
+        requests.put('http://192.168.0.106/0', data="{\"on\":false}")
         self.speak_dialog('test')
 
 
